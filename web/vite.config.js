b0708c339e530538b52d26c43036e8ce9b27a207
palc88-stack <pal.c88@gmail.com>
2026-09-25 02:35:14 +0300

    Fix formatting issues in vite.config.js

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,17 +1,17 @@
-import { defineConfig } from \'vite\'
-import react from \'@vitejs/plugin-react\'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: \'dist\',
+    outDir: 'dist',
     sourcemap: false,
-    minify: \'esbuild\',
+    minify: 'esbuild',
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       output: {
         manualChunks: {
-          vendor: [\'react\', \'react-dom\'],
+          vendor: ['react', 'react-dom'],
         }
       }
     }
