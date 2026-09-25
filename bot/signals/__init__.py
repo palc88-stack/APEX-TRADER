e5c3c9249aa3d35b3ebb61212b51353d5df0e43d
palc88-stack <pal.c88@gmail.com>
2026-09-25 18:18:39 +0300

    Refactor imports and update __all__ exports
    Updated imports and __all__ exports to include new strategies and an indicator calculator.

--- bot/signals/__init__.py
+++ bot/signals/__init__.py
@@ -1,18 +1,17 @@
-# bot/signals/__init__.py
-from bot.signals.indicators import TechnicalIndicators
-from bot.signals.signal_engine import (
-    SignalEngine,
-    TradeSignal,
-    TradeDirection,
-    TradingMode
-)
+from bot.signals.indicators import IndicatorCalculator, TechnicalIndicators
 from bot.signals.filters import SignalFilters
+from bot.signals.signal_engine import SignalEngine, TradeSignal, TradeDirection, TradingMode
+from bot.signals.explosion import ExplosionStrategy
+from bot.signals.scalping import ScalpingStrategy
 
 __all__ = [
+    "IndicatorCalculator",
     "TechnicalIndicators",
+    "SignalFilters",
     "SignalEngine",
     "TradeSignal",
     "TradeDirection",
     "TradingMode",
-    "SignalFilters"
+    "ExplosionStrategy",
+    "ScalpingStrategy"
 ]
