fb6f072ee74385994a2c7dee733dbc608350cbbc
palc88-stack <pal.c88@gmail.com>
2026-09-25 03:18:31 +0300

    Remove chunkSizeWarningLimit and server port settings

--- web/vite.config.js
+++ web/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from \'vite\'
 import react from \'@vitejs/plugin-react\'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: \'dist\',
-    sourcemap: false,
-    chunkSizeWarningLimit: 1600
-  },
-  server: {
-    port: 3000
+    sourcemap: false
   }
 })
